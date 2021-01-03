82c1b2da077c47fe80ae4db68af416aa7aed15f1
poornima-whjr <73928793+poornima-whjr@users.noreply.github.com>
2021-01-03 12:45:56 +0530

    Update Food.js

--- js/Food.js
+++ js/Food.js
@@ -1,6 +1,6 @@
 class Food{
     constructor(){
-        this.image = loadImage("../images/Milk.png");
+        this.image = loadImage("./images/Milk.png");
         this.foodStock;
         this.lastFed;
         this.lastBottleX;
